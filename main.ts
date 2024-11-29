5f9ca67dea9e9fc76e46c1027274369e3aa9aca4
Maxim Leonkin <maksimleonkin583@gmail.com>
2024-11-29 21:43:02 +0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -13,17 +13,16 @@ const web_hook_path = `/${bot.token}/webhook`;
 app.post(web_hook_path, async (req: Request, res: Response) => {
     if (req.method === "POST" && req.path === web_hook_path) {
         try {
-            await handleUpdate(req, res); // Передаем запрос и ответ в обработчик обновлений
+            await handleUpdate(req, res);
         } catch (err) {
             console.error(err);
-            res.sendStatus(500); // Ответ с ошибкой
+            res.sendStatus(500);
         }
     } else {
-        res.sendStatus(404); // Если путь не соответствует токену бота или метод не POST
+        res.sendStatus(404);
     }
 });
 
-// Слушаем на порту 5000
 const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
