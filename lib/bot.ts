869488f08f6596aa05abff3c700774866b9ce2aa
Maxim Leonkin <maksimleonkin583@gmail.com>
2024-12-08 15:31:28 +0600

    Update bot.ts

--- lib/bot.ts
+++ lib/bot.ts
@@ -147,7 +147,7 @@ bot.callbackQuery("hobby", async (ctx) => {
 bot.callbackQuery("coffeeshop", async (ctx) => {
     await ctx.answerCallbackQuery();
     await ctx.deleteMessage();
-    await ctx.reply("Укажите новую геопозицию кофейни с помощью встроенного инструмента Telegram(📎-> 🚩Геопозиция).");
+    await ctx.reply("Укажите новую геопозицию кофейни с помощью встроенного инструмента Telegram.\n📎-> 🚩Геопозиция");
     info.status = "editCoffeeshop";
 });
 
@@ -268,7 +268,7 @@ bot.on("message", async (ctx) =>{
                 if (ctx.msg.text){
                     info.hobby = ctx.msg.text.split(",");
                     info.status = "createCoffeeshop";
-                    await ctx.reply("Теперь укажите геопозицию вашей любимой кофейни с помощью встроенного инструмента Telegram(📎-> 🚩Геопозиция).");
+                    await ctx.reply("Теперь укажите геопозицию вашей любимой кофейни с помощью встроенного инструмента Telegram.\n📎-> 🚩Геопозиция");
                 }
                 break;
 
@@ -279,7 +279,7 @@ bot.on("message", async (ctx) =>{
                     info.status = "createTime";
                     await ctx.reply("Напишите удобное время для встречи.");
                 } else {
-                    await ctx.reply("Укажите геопозицию с помощью встроенного инструмента Telegram");
+                    await ctx.reply("Укажите геопозицию с помощью встроенного инструмента Telegram.\n📎-> 🚩Геопозиция");
                 }
                 break;
 
